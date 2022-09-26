ad4d89d65d940c516b4e7ae9c6bce929cd8f1b68
Programxiaoming <xiaomingsu@outlook.com>
2022-09-26 01:53:26 -0400

    update all files again

---
+++ includes/banner.php
@@ -0,0 +1,19 @@
+<div class="banner">
+	<div class="welcome_msg">
+		<h1>Today's Aphorisms</h1>
+		<p> 
+			I do not fear computers.<br> 
+			I fear lack of them.<br> 
+			<span>~ Isaac Asimov</span>
+		</p>
+		<a href="register.php" class="btn">register here!</a>
+	</div>
+	<div class="login_div">
+		<form action="index.php" method="post" >
+			<h2>Login</h2>
+			<input type="text" name="username" placeholder="Username">
+			<input type="password" name="password"  placeholder="Password"> 
+			<button class="btn" type="submit" name="login_btn">Sign in</button>
+		</form>
+	</div>
+</div>
